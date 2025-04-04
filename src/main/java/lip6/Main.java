035663dc95cac388f14e69eab66fa1d7e2d4e020
milal ziane <mikal.ziane@gmail.com>
2025-04-04 18:09:13 +0200

    essai spoon

--- src/main/java/lip6/Main.java
+++ src/main/java/lip6/Main.java
@@ -1,7 +1,17 @@
 package lip6;
 
+import spoon.Launcher;
+import spoon.SpoonAPI;
+import spoon.reflect.CtModel;
+import spoon.reflect.declaration.CtClass;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpoonAPI spoon = new Launcher();
+        spoon.addInputResource("D:\\repos\\EssaiSpoon\\src\\main\\java\\lip6\\Main.java");
+        CtModel model = spoon.buildModel();
+
     }
 }
+
